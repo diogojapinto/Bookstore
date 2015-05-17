3e88fdbccb6bd5e3ea90d3046d081c0629021742
diogojapinto <diogojapinto@gmail.com>
2015-05-17 12:34:37 +0100

    added javafx applications

--- Store/src/java/com/book/store/api/ApplicationConfig.java
+++ Store/src/java/com/book/store/api/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.book.store.api.service.BookFacadeREST.class);
+        resources.add(com.book.store.api.service.OrdersREST.class);
     }
     
 }
