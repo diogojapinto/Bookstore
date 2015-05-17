4bcf4f52927c5d69e156cac0c9f3db1656b99a93
diogojapinto <diogojapinto@gmail.com>
2015-05-17 12:58:38 +0100

    some more tests

--- StoreClient/src/java/storeclient/FXMLDocumentController.java
+++ StoreClient/src/java/storeclient/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication3;
+package storeclient;
 
 import java.net.URL;
 import java.util.ResourceBundle;
