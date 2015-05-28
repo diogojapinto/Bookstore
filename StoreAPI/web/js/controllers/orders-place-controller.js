8773a1850b26d9889d8504cb4b4dd0269ccf3b21
diogojapinto <diogojapinto@gmail.com>
2015-05-28 13:55:15 +0100

    almost there

--- StoreAPI/web/js/controllers/orders-place-controller.js
+++ StoreAPI/web/js/controllers/orders-place-controller.js
@@ -1,5 +1,6 @@
 angular.module('BookStore').controller('PlaceOrderController', function ($scope, Store) {
     var waiting = true;
+    $scope.order = {};
     Store.getBooksList()
             .success(function (data, status, headers, config) {
                 $scope.books = data;
@@ -11,7 +12,25 @@ angular.module('BookStore').controller('PlaceOrderController', function ($scope,
             });
 
     $scope.placeOrder = function () {
-        // all names are already in scope (or model, whichever you prefer)
-        // maybe redirect to view orders
+        var newOrder = {
+            title: $scope.selectedBook.title,
+            quantity: $scope.book.quantity,
+            clientName: $scope.user.name,
+            clientEmail: $scope.user.email,
+            clientAddress: $scope.user.address
+        };        
+        
+        Store.placeOrder(newOrder)
+                .success(function (data, status, headers, config) {
+                    $scope.order.success = true;
+                    $scope.order.error = false;
+                    $scope.order.message = "Order successfully placed";
+                })
+                .error(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.order.success = false;
+                    $scope.order.error = true;
+                    $scope.order.message = "There was an error placing the order";
+                });
     };
 });
--- StoreAPI/web/js/services/store.js
+++ StoreAPI/web/js/services/store.js
@@ -3,8 +3,9 @@ angular.module('BookStore').factory('Store', function ($http) {
         getUserOrders: function (id) {
             return $http({method: 'GET', url: '/StoreAPI/webresources/orders/' + id});
         },
-        placeOrder: function () {
-            return $http({method: 'POST', url: '/StoreAPI/webresources/orders/'});
+        placeOrder: function (newOrder) {
+            return $http({method: 'POST', url: '/StoreAPI/webresources/orders/true',
+                data: newOrder});
         },
         getBooksList: function () {
             return $http({method: 'GET', url: '/StoreAPI/webresources/books/'});
