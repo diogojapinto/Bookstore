f532e6a8c5cc60e5ecda8eaf3be93a054b9196f2
diogojapinto <diogojapinto@gmail.com>
2015-05-28 10:04:07 +0100

    services are available

--- StoreAPI/web/js/controllers/orders-place-controller.js
+++ StoreAPI/web/js/controllers/orders-place-controller.js
@@ -1,6 +1,17 @@
-angular.module('BookStore').controller('PlaceOrderController', function($scope, Store) {
-  $scope.placeOrder = function() {
-    // all names are already in scope (or model, whichever you prefer)
-    // maybe redirect to view orders
-  };
+angular.module('BookStore').controller('PlaceOrderController', function ($scope, Store) {
+    var waiting = true;
+    Store.getBooksList()
+            .success(function (data, status, headers, config) {
+                $scope.books = data;
+                waiting = false;
+            })
+            .error(function (data, status, headers, config) {
+                console.log(data);
+                waiting = false;
+            });
+
+    $scope.placeOrder = function () {
+        // all names are already in scope (or model, whichever you prefer)
+        // maybe redirect to view orders
+    };
 });
--- StoreAPI/web/js/services/store.js
+++ StoreAPI/web/js/services/store.js
@@ -1,10 +1,13 @@
-angular.module('BookStore').factory('Store', function NoteFactory() {  
-  return {
-    getUserOrders: function(id) {
-      return $http({method: 'GET', url: '/webresources/orders/' + id});
-    },
-    placeOrder: function() {
-      return $http({method: 'POST', url: '/webresources/orders/'});
-    }, 
-  };
+angular.module('BookStore').factory('Store', function ($http) {
+    return {
+        getUserOrders: function (id) {
+            return $http({method: 'GET', url: '/StoreAPI/webresources/orders/' + id});
+        },
+        placeOrder: function () {
+            return $http({method: 'POST', url: '/StoreAPI/webresources/orders/'});
+        },
+        getBooksList: function () {
+            return $http({method: 'GET', url: '/StoreAPI/webresources/books/'});
+        }
+    };
 });
